55291ee633412de73e47ebd80e2c8f30865d1f26
wangyuxin <wangyuxin@mokahr.com>
2022-10-09 17:22:35 +0800

    🐞fix:fix date bug & bump version

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import datetime
 from hypothesis import given, note
 from hypothesis import strategies as st
 from packaging.version import parse as version_parse
@@ -20,7 +20,7 @@ tag_string_strategy = st.from_regex(VALID_TAG_PATTERN, fullmatch=True)
     stem=st.from_regex(STEM_PATTERN, fullmatch=True),
     suffix=st.one_of(st.none(), st.from_regex(VALID_SUFFIX_PATTERN, fullmatch=True)),
     tags=st.one_of(st.none(), tag_string_strategy, st.lists(tag_string_strategy, min_size=1)),
-    date=st.one_of(st.none(), st.booleans(), st.dates(), st.datetimes()),
+    date=st.one_of(st.none(), st.booleans(), st.dates(min_value=datetime.date(2000, 1, 1)), st.datetimes()),
     version=st.one_of(st.none(), st.just('1.0.1.post1'), st.just('2.0')),
 )
 def test_file_name(stem, suffix, tags, date, version):
