6d42cdbf1c249280c95604282e03cbb0a4f8edbc
wangyuxin <wangyuxin@mokahr.com>
2022-10-11 17:47:50 +0800

    🐞fix:doctest bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 project = 'namefile'
 copyright = '2022, wangyuxin'
 author = 'wangyuxin'
